09cc98221e128aaffc25ad0feb17ccf120dad1a4
Saloni Rathore <salonirathore091@gmail.com>
2026-03-17 23:24:33 +0530

    remove spaces

---
+++ String/RemoveSpaces.java
@@ -0,0 +1,14 @@
+public class RemoveSpaces {
+    public static void main(String args[]){
+        String str="hello world";
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)!=' '){
+           sb.append(str.charAt(i));
+            }
+        }
+        System.out.println("With Spaces :"+str);
+        System.out.println("Removed Spaces :"+sb.toString());
+    }
+}
+//Note we can also use replace method.
